84da38634d50586939e46adb3fb956e9e9ecbd1a
Somesh Debnath <91829500+Somesh-Debnath@users.noreply.github.com>
2022-07-05 00:04:20 +0530

    Time: 3 ms (76.43%), Space: 42.3 MB (56.98%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,24 +1,6 @@
- public class Solution {
-    public boolean is_Interleave(String s1, int i, String s2, int j, String s3, int k, int[][] memo) {
-        if (i == s1.length()) {
-            return s2.substring(j).equals(s3.substring(k));
-        }
-        if (j == s2.length()) {
-            return s1.substring(i).equals(s3.substring(k));
-        }
-        if (memo[i][j] >= 0) {
-            return memo[i][j] == 1 ? true : false;
-        }
-        boolean ans = false;
-        if (s3.charAt(k) == s1.charAt(i) && is_Interleave(s1, i + 1, s2, j, s3, k + 1, memo)
-                || s3.charAt(k) == s2.charAt(j) && is_Interleave(s1, i, s2, j + 1, s3, k + 1, memo)) {
-            ans = true;
-        }
-        memo[i][j] = ans ? 1 : 0;
-        return ans;
-    }
+class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) {
+         if (s1.length() + s2.length() != s3.length()) {
             return false;
         }
         int memo[][] = new int[s1.length()][s2.length()];
@@ -27,6 +9,19 @@
                 memo[i][j] = -1;
             }
         }
-        return is_Interleave(s1, 0, s2, 0, s3, 0, memo);
+        return isInterleaved(s1, 0, s2, 0, s3, 0, memo);
+    }
+    public boolean isInterleaved(String s1,int i,String s2,int j,String s3,int k,int [][]memo){
+        if(i==s1.length())
+            return s2.substring(j).equals(s3.substring(k));
+        if(j==s2.length())
+            return s1.substring(i).equals(s3.substring(k));
+        if(memo[i][j]>=0)
+            return memo[i][j]==1?true:false;
+        boolean ans=false;
+        if(s3.charAt(k)==s1.charAt(i) && isInterleaved(s1,i+1,s2,j,s3,k+1,memo) || s3.charAt(k)==s2.charAt(j) && isInterleaved(s1,i,s2,j+1,s3,k+1,memo))
+            ans=true;
+         memo[i][j] = ans ? 1 : 0;
+        return ans;
     }
 }
