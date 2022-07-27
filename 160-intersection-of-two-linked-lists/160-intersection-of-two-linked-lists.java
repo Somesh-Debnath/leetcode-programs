ba8a3b9e6e4aebd109be2df00ad5dfdfb57fd0bb
Somesh Debnath <91829500+Somesh-Debnath@users.noreply.github.com>
2022-07-27 12:53:09 +0530

    Time: 1 ms (99.69%), Space: 55.5 MB (41.72%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -10,19 +10,13 @@
  * }
  */
 public class Solution {
-public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-    //boundary check
-    if(headA == null || headB == null) return null;
-    
-    ListNode a = headA;
-    ListNode b = headB;
-    
-    //if a & b have different len, then we will stop the loop after second iteration
-    while( a != b){
-    	//for the end of first iteration, we just reset the pointer to the head of another linkedlist
-        a = a == null? headB : a.next;
-        b = b == null? headA : b.next;    
-    }
-    
-    return a;
-}}
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pt1=headA,pt2=headB;
+        while(pt1!=pt2){
+           pt1=pt1==null?headB:pt1.next;
+            pt2=pt2==null?headA:pt2.next;
+       
+}
+        return pt1;
+}
+}
